17f8da9d35f343fac37d5279998c43a3f0a17554
tomito26 <tommybwah@gmail.com>
2021-07-21 13:09:35 +0300

    created a new project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operations]");
+const equalBtn =  document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperand = document.querySelector("[data-previous-operand]");
+const currentOperand = document.querySelector("[data-current-operand]");
+
